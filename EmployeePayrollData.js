99ca01f8bb835348d89efec565c00b8f38d76fe0
santhoshkn011 <santhoshkn011@gmail.com>
2022-08-04 21:08:56 +0530

    Added: Employee payroll data.

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -1,1 +1,24 @@
-console.log("Welcome to the Employee Payroll Data Program.");
+class EmployeePayrollData {
+    //Constructor
+
+    constructor(id, name, salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    //Getter And Setter
+    get name() {
+        return this._name;
+    }
+    set name(name) {
+        this._name = name;
+    }
+
+    //Method
+    toString() {
+        return "id: " + this.id + "\nname: " + this.name + "\nSalary: " + this.salary;
+    }
+} 
+    let employeePayrollData = new EmployeePayrollData(11, "Santhosh", 30000);
+console.log(employeePayrollData.toString());
