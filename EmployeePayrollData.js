216f8032541dfcf90e57ae3a77a85cf272f57942
santhoshkn011 <santhoshkn011@gmail.com>
2022-08-05 17:08:22 +0530

    Added: Check regex pattern, used try and catch

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -1,7 +1,6 @@
 /*
-Ability to check the name
-starts with capital and has at
-least 3 characters - Use Regex Pattern - Use Try Catch in case of Error
+Ability to check the employee id and salary are non zero positive number, the gender is M or F and
+date is not a future date
 */
 class EmployeePayrollData {
     
@@ -11,11 +10,23 @@ class EmployeePayrollData {
         this.id = empData[0];
         this.name = empData[1];
         this.salary = empData[2];
-        this.gender = empData[3];
+        this.genders = empData[3];
         this.startDate = empData[4];
     }
 
     //Getter And Setter
+    get id() {
+        return this._id;
+    }
+
+    set id(id) {
+        let idRegx = RegExp('^[1-9][0-9]*$');
+        if (idRegx.test(id))
+            this._id = id;
+        else throw 'Invalid ID';
+            
+    }
+
     get name() {
         return this._name;
     }
@@ -23,31 +34,86 @@ class EmployeePayrollData {
         let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
         if (nameRegex.test(name))
             this._name = name;
-        else throw 'Name is incorrect!!';
+        else throw 'Invalid Name';
+    }
+
+    get salary() {
+        return this._salary;
+    }
+
+    set salary(salary) {
+        let salaryRegx = RegExp('^[1-9][0-9]*$');
+        if (salaryRegx.test(salary))
+            this._salary = salary;
+        else throw 'Invalid Salary.';
+    }
+
+    get genders() {
+        return this._genders;
+    }
+
+    set genders(genders) {
+        let genderRegex = RegExp('^(?:m|M|male|Male|f|F|female|Female)$');
+        if (genderRegex.test(genders)){
+            this._genders = genders;
+        }   
+        else {
+            throw 'Invalid gender';  
+        }      
+    }
+
+    get startDate() {
+        return this._startDate;
+    }
+
+    set startDate(startDate) {
+        if(startDate <= new Date())
+            this._startDate = startDate;
+        else throw 'Invalid date';         
     }
 
     //Method
     toString() {     
-        return "id: " + this.id + "\nname: " + this.name + "\nSalary: " + this.salary + "\nGender: " +this.gender+ "\nStartDate: " + this.startDate;
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const employeeDate = this.startDate == undefined ? "undefined" : this.startDate.toLocaleDateString("en-us", options);
+        return "Id = " + this.id + ", Name = " + this.name + ", Gender = " + this.genders + ", Salary = " + this.salary + ", Start Date = " + employeeDate;
     }
 } 
 /*
 toLocaleDateString() returns the date (not the time) of a date, as a string, using locale conventions.
 */
-const date = new Date();
-let currentDate = date.toLocaleDateString();
 
-let employeePayrollData1 = new EmployeePayrollData(1, "Santhosh", 30000);
-console.log(employeePayrollData1.toString());
-console.log();
+// const date = new Date();
+// let currentDate = date.toLocaleDateString();
 
 try {
-    let employeePayrollData2 = new EmployeePayrollData(2, "kumar", 35000);
-    console.log(employeePayrollData2.toString());  
-}catch (e) {
-    console.error(e);
+    let employeePayrollData1 = new EmployeePayrollData(1, "Santhosh", 30000, "M", new Date());
+    console.log(employeePayrollData1.toString());
+}
+catch (e) {
+    console.error("An error caught: "+e);
 }
 
-console.log();
-let employeePayrollData3 = new EmployeePayrollData(3 , "Nayak" , 40000 , "M" , currentDate);
-console.log(employeePayrollData3.toString());
+try {
+    let teresaPayrollData = new EmployeePayrollData(0, "Namitha", 50000, "F", new Date());
+    console.log(teresaPayrollData.toString()); 
+}
+catch (e) {
+    console.error("An error caught: "+e);
+}
+
+try {
+    let employeePayrollData = new EmployeePayrollData(2, "Kumar", 5000000, "M", new Date());
+    console.log(employeePayrollData.toString()); 
+}
+catch (e) {
+    console.error("An error caught: "+e);
+}
+
+try {
+    let teresaPayrollData = new EmployeePayrollData(3, "Nayak", 50000, "Mal", new Date());
+    console.log(teresaPayrollData.toString()); 
+}
+catch (e) {
+    console.error("An error caught: "+e);
+}
